6c2c77cd96e0fb58fb8940d22ae16b5f9b34d7d2
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-01-05 08:21:25 +0530

    level order bfs

--- src/BinaryTree/Traversal/BFS.java
+++ src/BinaryTree/Traversal/BFS.java
@@ -4,17 +4,45 @@ import BinaryTree.Node;
 import java.util.LinkedList;
 import java.util.Queue;
 
+class Pair{
+    Node node;
+    int level;
+    Pair (Node node, int level){
+        this.node = node;
+        this.level = level;
+    }
+}
+
 public class BFS {
     public static void bfs(Node root){
         Queue<Node> queue = new LinkedList<>();
         queue.add(root);
         while(!queue.isEmpty()){
             Node front = queue.remove();
-            System.out.print(front.val + " ");
+            System.out.println(front.val + " ");
             if(front.left != null) queue.add(front.left);
             if(front.right != null) queue.add(front.right);
         }
         System.out.println();
+    }
+
+    public static void levelOrderBfs(Node root){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root, 0));
+        int currLevel = 0;
+
+        while(!q.isEmpty()){
+            Pair front = q.remove();
+            Node node = front.node;
+            int level = front.level;
+            if(front.level != currLevel){
+                currLevel++;
+                System.out.println();
+            }
+            System.out.print(node.val + " ");
+            if(node.left != null) q.add(new Pair(node.left, level + 1));
+            if(node.right != null) q.add(new Pair(node.right, level + 1));
+        }
     }
     public static void main(String[] args) {
         Node a = new Node(3);
@@ -32,6 +60,6 @@ public class BFS {
         c.left = f;
         c.right = g;
 
-        bfs(a);
+        levelOrderBfs(a);
     }
 }
