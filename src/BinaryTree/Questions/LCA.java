3145885ce86355db133f8e60e7874abcf33f824e
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-01-11 07:34:47 +0530

    lca

--- src/BinaryTree/Questions/LCA.java
+++ src/BinaryTree/Questions/LCA.java
@@ -1,4 +1,45 @@
 package BinaryTree.Questions;
 
+import BinaryTree.Node;
+
 public class LCA {
+    public static boolean  isExistA;
+    public static boolean isExistB;
+    public static Node lowestCommonAncestor(Node root, int p, int q){
+        if(root == null) return null;
+        if(root.val == p || root.val == q) return root;
+        boolean pLiesInLeft = exists(root.left, p);
+        boolean qLiesInLeft = exists(root.right, q);
+        if(pLiesInLeft && qLiesInLeft) return lowestCommonAncestor(root.left, p, q);
+        if(!pLiesInLeft && !qLiesInLeft) return lowestCommonAncestor(root.right, p, q);
+        else return root;
+    }
+
+    private static boolean exists(Node root, int val) {
+        if(root == null )return false;
+        if(root.val == val) return true;
+        return exists(root.left, val) || exists(root.right, val);
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(3);
+        Node b = new Node(4);
+        Node c = new Node(2);
+        Node d = new Node(-1);
+        Node e = new Node(1);
+        Node f = new Node(6);
+        Node g = new Node(9);
+
+
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+        c.right = g;
+
+        Node lca = lowestCommonAncestor(a, 6, 9);
+        System.out.println(lca.val);
+    }
 }
