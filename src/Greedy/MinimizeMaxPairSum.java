023822fe55caca1966420094069edf5c99b4f415
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-01-24 22:36:45 +0530

    leetcode potd

---
+++ src/Greedy/MinimizeMaxPairSum.java
@@ -0,0 +1,15 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class MinimizeMaxPairSum {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0;
+        int i = 0, j = nums.length - 1;
+        while(i < j){
+            max  = Math.max(max, nums[i++] + nums[j--]);
+        }
+        return max;
+    }
+}
