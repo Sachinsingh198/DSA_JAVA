994593a7e70d3a69b724d34780af07905aae2d6a
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-01-30 07:13:22 +0530

    kth smallest in array

--- src/Heaps/KthSmallestElement.java
+++ src/Heaps/KthSmallestElement.java
@@ -1,8 +1,16 @@
 package Heaps;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class KthSmallestElement {
     public static int kthSmallest(int[] arr, int k){
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int el : arr){ // n * log k
+            pq.add(el); // log k
+            if(pq.size() > k) pq.remove();
+        }
+        return pq.peek();
 
     }
     public static void main(String[] args) {
