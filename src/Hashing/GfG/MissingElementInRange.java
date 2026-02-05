f30089b0c36d07a9ef4e5d0f0198ff8ca378fecf
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-05 07:03:03 +0530

    leetcode wala

--- src/Hashing/GfG/MissingElementInRange.java
+++ src/Hashing/GfG/MissingElementInRange.java
@@ -3,6 +3,7 @@ package Hashing.GfG;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class MissingElementInRange {
     public static ArrayList<Integer> missingRange(int[] arr, int low, int high){
@@ -12,6 +13,21 @@ public class MissingElementInRange {
             set.add(el);
         }
         for(int i = low; i <= high; i++){
+            if(!set.contains(i)) ans.add(i);
+        }
+        return ans;
+    }
+    public List<Integer> findMissingElements(int[] arr) {
+        List<Integer> ans = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int el : arr){
+            max = Math.max(max, el);
+            min = Math.min(min, el);
+            set.add(el);
+        }
+        for(int i = min; i <= max; i++){
             if(!set.contains(i)) ans.add(i);
         }
         return ans;
