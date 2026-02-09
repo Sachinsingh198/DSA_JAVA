8b0c611918d2e17caa4d63235471fe524d458ac6
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-09 23:05:56 +0530

    hashmap in trees

---
+++ src/Hashing/GfG/TimeToBurnTree.java
@@ -0,0 +1,13 @@
+package Hashing.GfG;
+
+import BinaryTree.Node;
+
+public class TimeToBurnTree {
+    public int amountOfTime(Node root, int start) {
+
+        return 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
