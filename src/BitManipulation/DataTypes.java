198e552ff9abfecda4d34e814e96325e7626f713
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-12 21:03:49 +0530

    leetcode potd

---
+++ src/BitManipulation/DataTypes.java
@@ -0,0 +1,14 @@
+package BitManipulation;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Range => -128 to 127
+        byte x = 127;
+        x += 1;
+        System.out.println(x);
+
+        int y = Integer.MAX_VALUE;
+        y += 10;
+        System.out.println(y);
+    }
+}
