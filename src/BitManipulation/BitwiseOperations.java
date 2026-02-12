fffcd497fd056befa8925e7f464faa14f992a3c6
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-12 07:43:49 +0530

    bitwise manipulation

---
+++ src/BitManipulation/BitwiseOperations.java
@@ -0,0 +1,10 @@
+package BitManipulation;
+
+public class BitwiseOperations {
+    public static void main(String[] args) {
+        System.out.println(151 & 98);
+        System.out.println(151 | 98);
+        System.out.println(151 ^ 98);
+        System.out.println(~(151 ^ 98));
+    }
+}
