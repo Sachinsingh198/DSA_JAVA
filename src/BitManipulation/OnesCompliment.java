a2d99e3ca483ffad08eaa3a4450649b655ec01f2
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-12 08:28:26 +0530

    other techniques

--- src/BitManipulation/OnesCompliment.java
+++ src/BitManipulation/OnesCompliment.java
@@ -4,5 +4,6 @@ public class OnesCompliment {
     public static void main(String[] args) {
         int x = 10;
         System.out.println(~x);
+        System.out.println(-x + 1); // 2s compliment + 1
     }
 }
--- src/BitManipulation/TwosCompliment.java
+++ src/BitManipulation/TwosCompliment.java
@@ -3,7 +3,7 @@ package BitManipulation;
 public class TwosCompliment {
     public static void main(String[] args) {
         int x = 7;
-        System.out.println(~x + 1);
+        System.out.println(~x + 1); // Ones compliment + 1
         System.out.println(-x);
     }
 }
