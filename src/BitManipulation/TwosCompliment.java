adc1f4ddd5011985651184e1088fe3eb775f04c8
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-12 08:26:18 +0530

    twos compliment

---
+++ src/BitManipulation/TwosCompliment.java
@@ -0,0 +1,9 @@
+package BitManipulation;
+
+public class TwosCompliment {
+    public static void main(String[] args) {
+        int x = 7;
+        System.out.println(~x + 1);
+        System.out.println(-x);
+    }
+}
