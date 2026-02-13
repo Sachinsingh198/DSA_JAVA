513367662fbe1b2898489bfda4a822726f1aa87e
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-13 08:22:08 +0530

    set kth bit

--- src/BitManipulation/SetKthBit.java
+++ src/BitManipulation/SetKthBit.java
@@ -2,9 +2,6 @@ package BitManipulation;
 
 public class SetKthBit {
     static int setKthBit(int n, int k) {
-        // code here
-        int x = 1;
-        x = x << k;
-        return n | x;
+        return n | (1 << k);
     }
 }
