db48d34b7adb3c21aeaba49ae33aede5808dc9ca
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-16 07:06:31 +0530

    xor from left to right in a given range

---
+++ src/BitManipulation/XorFromLtoR.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class XorFromLtoR {
+    public static int xor(int n){
+        if(n % 4 == 1) return 1;
+        else if(n % 4 == 2) return n +1;
+        else if(n % 4 == 3) return 0;
+        else return n;
+    }
+    public static int findXOR(int l, int r){
+        return xor(l - 1) ^ xor(r);
+    }
+    public static void main(String[] args) {
+
+    }
+}
