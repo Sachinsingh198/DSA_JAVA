4417a2b475877d786eccc8619052f26892b1facb
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-19 21:53:00 +0530

    count binary substring

---
+++ src/BitManipulation/CountSubstring.java
@@ -0,0 +1,20 @@
+package BitManipulation;
+
+public class CountSubstring {
+    public int countBinarySubstrings(String s) {
+        int ans = 0;
+        int currcount = 1, prevcount = 0;
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i - 1)){
+                currcount++;
+            }
+            else{
+                prevcount = currcount;
+                currcount = 1;
+            }
+            if(currcount <= prevcount) ans++;
+        }
+
+        return ans;
+    }
+}
