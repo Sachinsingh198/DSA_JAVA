72bdc7216e9b973c3baeb75fe67731e17e5b2815
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-24 06:16:44 +0530

    optimized house robber

--- src/DP/Questions/HouseRobber.java
+++ src/DP/Questions/HouseRobber.java
@@ -4,6 +4,32 @@ import java.util.Arrays;
 
 public class HouseRobber {
     static int[] dp;
+    public static int robberyUsingTabulation(int[] arr){
+        int[] dpArr = new int[arr.length];
+        dpArr[0] = arr[0];
+        dpArr[1] = Math.max(arr[0], arr[1]);
+
+        for(int i = 2; i < arr.length; i++){
+            dpArr[i] = Math.max(dpArr[i - 1], arr[i] + dpArr[i - 2]);
+        }
+        return dp[arr.length - 1];
+    }
+    public int rob(int[] arr) {
+        if(arr.length == 1) return arr[0];
+
+        int prev1 = arr[0];
+        int prev2 = Math.max(arr[0], arr[1]);
+
+        for(int i = 2; i < arr.length; i++){
+            int curr = Math.max(prev2, arr[i] + prev1);
+            prev1 = prev2;
+            prev2 = curr;
+
+        }
+        return prev2;
+    }
+
+
     public static int helper(int[] arr, int idx){
         if(idx >= arr.length) return 0;
         if(dp[idx] != -1) return dp[idx];
--- src/Hashing/Questions/UnionOfArrays.java
+++ src/Hashing/Questions/UnionOfArrays.java
@@ -1,9 +1,19 @@
 package Hashing.Questions;
 
+import javax.naming.ldap.HasControls;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class UnionOfArrays {
     public static ArrayList<Integer> findUnion(int[] a, int[] b) {
-
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        for(int el : a){
+            set.add(el);
+        }
+        for(int el : b) set.add(el);
+        for(int el : set) ans.add(el);
+        return ans;
     }
 }
