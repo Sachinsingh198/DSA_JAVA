edaccc96b21183960cbf018ecf9a4fc3182db79c
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-24 07:24:11 +0530

    friends pairing problem

--- src/DP/Questions/FriendsPairingProblem.java
+++ src/DP/Questions/FriendsPairingProblem.java
@@ -18,4 +18,19 @@ public class FriendsPairingProblem {
         dp[n] = pair(n - 1) + (n - 1) * pair(n - 2);
         return dp[n];
     }
+    public static long friend(int n){
+
+        dp[1] = 1;
+        int prev1 = 1;
+        int prev2 = 0;
+
+        if(n > 1) prev2  = 2;
+        for(int i = 3; i <= n; i++){
+
+            int curr = prev2 + (i - 1) * prev1;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
 }
