cafb94ced52c473bf70949e72939cc8f8970bf74
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-25 08:03:12 +0530

    unique paths space optimized

--- src/DP/Questions/UniquePaths.java
+++ src/DP/Questions/UniquePaths.java
@@ -40,6 +40,23 @@ public class UniquePaths {
         return dp[m - 1][n - 1];
     }
 
+    public int uniquePathsSpaceOptimized(int m, int n) {
+        int[][] dp = new int[2][n];
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
+            dp[1][j] = 1;
+        }
+        for(int i = 1; i <=m-1; i++){
+            for(int j = 1; j < n; j++){
+                dp[1][j] = dp[1][j-1] + dp[0][j];
+            }
+            for(int j = 1 ; j < n; j++){
+                dp[0][j] = dp[1][j];
+            }
+        }
+        return dp[1][n - 1];
+    }
+
 
 
     public static void main(String[] args) {
