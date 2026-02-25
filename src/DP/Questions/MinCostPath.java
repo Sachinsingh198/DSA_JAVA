59a50ce025348c2c140ff26bf95fa07cfdb31da8
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-02-25 06:47:38 +0530

    min cost path optimized

--- src/DP/Questions/MinCostPath.java
+++ src/DP/Questions/MinCostPath.java
@@ -25,7 +25,26 @@ public class MinCostPath {
         return dp[m - 1][n - 1];
     }
 
+    public int minPathSumOptimized(int[][] grid) {
 
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for(int i = 1 ; i < m; i++){
+            grid[i][0] = grid[i - 1][0] + grid[i][0];
+        }
+        for(int j = 1; j < n; j++){
+            grid[0][j] = grid[0][j - 1] + grid[0][j];
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                grid[i][j] = grid[i][j] + Math.min(grid[i][j - 1], grid[i - 1][j]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
 
     public static void main(String[] args) {
 
