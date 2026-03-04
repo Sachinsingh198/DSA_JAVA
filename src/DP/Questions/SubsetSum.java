3275713d02bd9620c85ba8ac0a4ee0b6efcfcafd
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-04 08:06:55 +0530

    target sum using tabulation

--- src/DP/Questions/SubsetSum.java
+++ src/DP/Questions/SubsetSum.java
@@ -2,35 +2,60 @@ package DP.Questions;
 
 public class SubsetSum {
 
-    private static boolean subset(int idx,int[] arr, int target, int[][] dp) {
-        if(idx == arr.length){
+    private static boolean subset(int i,int[] nums, int target, int[][] dp) {
+        if(i == nums.length){
             if(target == 0) return true;
             else return false;
         }
-        if(dp[idx][target] != -1) return dp[idx][target] ==1;
+        if(dp[i][target] != -1) return dp[i][target] ==1;
         boolean ans = false;
-        boolean skip = subset(idx + 1,  arr, target, dp);
-        if(target - arr[idx] < 0  ) {
+        boolean skip = subset(i + 1,  nums, target, dp);
+        if(target - nums[i] < 0  ) {
             ans = skip;
         }
         else {
-            boolean pick = subset(idx + 1, arr, target - arr[idx], dp);
+            boolean pick = subset(i + 1, nums, target - nums[i], dp);
             ans = pick || skip;
         }
-        dp[idx][target] = (ans) ? 1 : 0;
+        dp[i][target] = (ans) ? 1 : 0;
         return ans;
     }
 
+    public static boolean canPartition(int[] nums){
+        int sum = 0, n = nums.length;
+        for(int el : nums) sum += el;
+        if(sum % 2 != 0) return false;
+
+        int target = sum / 2;
+        int[][] dp = new int[n][target + 1];
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                boolean ans = false;
+                boolean skip = (i > 0) ? (dp[i-1][j] == 1) :(j == 0) ;
+                if(j - nums[i] < 0  ) {
+                    ans = skip;
+                }
+                else {
+                    boolean pick = (i > 0) ? (dp[i-1][j - nums[i]] == 1) : j == 0 ;
+                    ans = pick || skip;
+                }
+                dp[i][j] = (ans) ? 1 : 0;
+            }
+        }
+
+        return dp[n-1][target] == 1;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {8, 5, 2, 4};
+        int[] nums = {8, 5, 2, 4};
         int target = 9;
-        int[][] dp = new int[arr.length][target + 1];
+        int[][] dp = new int[nums.length][target + 1];
         for(int i = 0; i < dp.length; i++){
             for(int j = 0; j < dp[0].length; j++){
                 dp[i][j] = -1;
             }
         }
-        System.out.println(subset(0, arr, target, dp));
+        System.out.println(subset(0, nums, target, dp));
     }
 
 
