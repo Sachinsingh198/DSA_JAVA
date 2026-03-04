9b898fd7a9213f89a98c077514145288bf3e3c92
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-04 08:24:54 +0530

    coin change

--- src/DP/Questions/CoinChange.java
+++ src/DP/Questions/CoinChange.java
@@ -14,11 +14,27 @@ public class CoinChange {
         return dp[i][amount] =  Math.min(skip, pick);
     }
     public int coinChange(int[] coins, int amount) {
-        long[][] dp = new long[coins.length][amount + 1];
-        for(int i = 0; i < dp.length; i++)
-            for(int j = 0; j < dp[0].length; j++)
-                dp[i][j] = -1;
-        int ans = (int) coinCount(0, coins, amount, dp);
+        int n = coins.length;
+        long[][] dp = new long[n][amount + 1];
+//        for(int i = 0; i < dp.length; i++)
+//            for(int j = 0; j < dp[0].length; j++)
+//                dp[i][j] = -1;
+//
+
+        // Using Tabulation
+        for(int i = 0; i < dp.length; i++){
+            for (int j = 0; j < dp[0].length; j++){
+                long skip =(i > 0) ?  dp[i-1][j] : (j == 0 ? 0 : Integer.MAX_VALUE);
+                if(j - coins[i] < 0)  dp[i][j] = skip;
+                else {
+                    long pick = 1 + dp[i][j - coins[i]];
+
+                     dp[i][j] = Math.min(skip, pick);
+                }
+            }
+        }
+
+        int ans = (int) dp[n-1][amount];
         return (ans == Integer.MAX_VALUE) ? -1 : ans;
     }
 }
