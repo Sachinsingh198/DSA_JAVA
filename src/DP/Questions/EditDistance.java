c1b659e5c5b4d84c81819e33984f2bff6394a1b8
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-10 07:34:47 +0530

    space optimized lcs print

--- src/DP/Questions/EditDistance.java
+++ src/DP/Questions/EditDistance.java
@@ -15,13 +15,23 @@ public class EditDistance {
             return  dp[i][j] =  1 + Math.min(del, Math.min(ins, rep));
         }
     }
-    public int minDistance(String word1, String word2) {
-        StringBuilder a = new StringBuilder(word1);
-        StringBuilder b = new StringBuilder(word2);
+    public int minDistance(String a, String b) {
         int m = a.length(), n = b.length();
+        if(m == 0) return n;
+        if(n == 0) return m;
         int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++) Arrays.fill(dp[i], -1);
-        return minSteps(m - 1, n - 1, a, b, dp);
+        for (int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int p = (i >= 1 && j >= 1) ? dp[i - 1][j - 1] : ((i == 0) ? j : i);
+                int q =(j >= 1) ? dp[i][j - 1] : i;
+                int r = (i >= 1) ?dp[i - 1][j] : j;
+                if(a.charAt(i) == b.charAt(j)) dp[i][j] = p;
+                else {
+                    dp[i][j] =  1 + Math.min(r, Math.min(q, p));
+                }
+            }
+        }
+        return dp[m - 1][n-1];
     }
     public static void main(String[] args) {
 
