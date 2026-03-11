d547680655cff185afc9a38c163cd7ca25b65424
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-11 23:39:59 +0530

    lc potd

---
+++ src/BitManipulation/Potd.java
@@ -0,0 +1,9 @@
+package BitManipulation;
+
+public class Potd {
+    public int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+        int mask = (1 << (32 - Integer.numberOfLeadingZeros(n))) - 1;
+        return n ^ mask;
+    }
+}
---
+++ src/DP/Questions/LongestCommonSupersequence.java
@@ -0,0 +1,7 @@
+package DP.Questions;
+
+public class LongestCommonSupersequence {
+    public String shortestCommonSupersequence(String str1, String str2) {
+
+    }
+}
