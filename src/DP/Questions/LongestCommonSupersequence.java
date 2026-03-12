6eea2fab0e1ff712cb46f2034bff674c03debf62
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-12 07:43:07 +0530

    longest common supersequence

--- src/DP/Questions/LongestCommonSupersequence.java
+++ src/DP/Questions/LongestCommonSupersequence.java
@@ -1,7 +1,63 @@
 package DP.Questions;
 
-public class LongestCommonSupersequence {
-    public String shortestCommonSupersequence(String str1, String str2) {
 
+public class LongestCommonSupersequence {
+    public static String LCS(String a, String b) {
+        int m = a.length(), n = b.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(a.charAt(i-1)==b.charAt(j-1))
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        int i = m, j = n;
+        while(i > 0 && j > 0){
+            if(a.charAt(i-1) == b.charAt(j-1)){
+                ans.append(a.charAt(i - 1));
+                i--;
+                j--
+            }
+
+            else if(dp[i-1][j] > dp[i][j - 1]) i--;
+            else j --;
+        }
+        ans.reverse();
+        return ans.toString();
+    }
+    public String shortestCommonSupersequence(String a, String b) {
+        String lcs = LCS(a, b);
+        int i = 0, j = 0, k = 0;
+        StringBuilder scs = new StringBuilder();
+        while(i < a.length() && j < b.length() && k < lcs.length()){
+            while(i < a.length() && a.charAt(i) != lcs.charAt(k)){
+                scs.append(a.charAt(i));
+                i++;
+            }
+            while(j < b.length() && b.charAt(j) != lcs.charAt(k)){
+                scs.append(b.charAt(j));
+                j++;
+            }
+            scs.append(lcs.charAt(k));
+            i++;
+            j++;
+            k++;
+        }
+        while(j < b.length()){
+            scs.append(b.charAt(j));
+            j++;
+        }
+        while(i < a.length()){
+            scs.append(a.charAt(i));
+            i++;
+        }
+
+
+
+        return scs.toString();
     }
 }
