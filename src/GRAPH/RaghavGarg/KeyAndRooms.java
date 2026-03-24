d859248ccd6561c51824618920bda5d06f08e5fb
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-24 07:07:19 +0530

    adding keys and rooms

---
+++ src/GRAPH/RaghavGarg/KeyAndRooms.java
@@ -0,0 +1,32 @@
+package GRAPH.RaghavGarg;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class KeyAndRooms {
+    public void Bfs(int i, List<List<Integer>> adj, boolean[] vis){
+        int n = adj.size();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+
+        while(!q.isEmpty()){
+            int front = q.remove();
+            for(int el : adj.get(front)){
+                if(!vis[el]){
+                    q.add(el);
+                    vis[el] = true;
+                }
+            }
+        }
+    }
+    public boolean canVisitAllRooms(List<List<Integer>> adj) {
+        int n = adj.size();
+
+        boolean[] vis = new boolean[n];
+        vis[0] = true;
+        Bfs(0, adj, vis);
+        for(boolean el : vis) if (!el) return false;
+        return true;
+    }
+}
