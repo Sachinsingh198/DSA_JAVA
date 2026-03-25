210bf31139e7dfb8b8bb27cde5e23f182c1caa47
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-25 07:43:26 +0530

    keys and rooms using dfs

--- src/GRAPH/RaghavGarg/KeyAndRooms.java
+++ src/GRAPH/RaghavGarg/KeyAndRooms.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Queue;
 
 public class KeyAndRooms {
+
+    // Using Bfs
     public void Bfs(int i, List<List<Integer>> adj, boolean[] vis){
         int n = adj.size();
         Queue<Integer> q = new LinkedList<>();
@@ -29,4 +31,24 @@ public class KeyAndRooms {
         for(boolean el : vis) if (!el) return false;
         return true;
     }
+
+
+    //Using DFS
+    private void dfs(int start, List<List<Integer>> adj, boolean[] vis) {
+        vis[start] = true;
+        for(int el : adj.get(start)){
+            if(!vis[el]) dfs(el, adj, vis);
+        }
+    }
+    public boolean canVisitAllRoomsDfs(List<List<Integer>> adj) {
+        int n = adj.size();
+
+        boolean[] vis = new boolean[n];
+        vis[0] = true;
+        dfs(0, adj, vis);
+        for(boolean el : vis) if (!el) return false;
+        return true;
+    }
+
+
 }
