77cfe2b93b5c7d056d583f30a1896768865b2731
Sachinsingh198 <sachinsinghfarswan123@gmail.com>
2026-03-25 07:20:09 +0530

    number of provinces dfs

--- src/GRAPH/RaghavGarg/NumberOfProvinces.java
+++ src/GRAPH/RaghavGarg/NumberOfProvinces.java
@@ -32,7 +32,33 @@ public class NumberOfProvinces {
         }
 
         return count;
-
     }
+
+    private void dfs(int i, boolean[] vis, int[][] adj) {
+        vis[i] = true;
+        for(int j = 0; j < adj.length; j++){
+            if(adj[i][j] == 1 && !vis[j]){
+                dfs(j, vis, adj);
+            }
+        }
+    }
+
+    public  int findCircleNumUsingDfs(int[][] adj) {
+        int n = adj.length;
+        int count = 0;
+
+        boolean[] vis = new boolean[n];
+
+        for(int i = 0; i  < n; i++){
+            if(!vis[i]) {
+                dfs(i, vis, adj);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
 
 }
